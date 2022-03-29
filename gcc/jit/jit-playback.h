b271b10248f4cd123be1f078ae3801b67651882a
Antoni Boucher <bouanto@zoho.com>
2022-03-28 21:10:41 -0400

    Add support for packed struct

--- gcc/jit/jit-playback.h
+++ gcc/jit/jit-playback.h
@@ -99,7 +99,8 @@ public:
   compound_type *
   new_compound_type (location *loc,
 		     const char *name,
-		     bool is_struct); /* else is union */
+		     bool is_struct, /* else is union */
+		     bool is_packed);
 
   type *
   new_function_type (type *return_type,
@@ -518,7 +519,7 @@ public:
     : type (inner)
   {}
 
-  void set_fields (const auto_vec<field *> *fields);
+  void set_fields (const auto_vec<field *> *fields, bool is_packed);
 };
 
 class field : public wrapper
--- gcc/jit/jit-recording.cc
+++ gcc/jit/jit-recording.cc
@@ -2506,6 +2506,12 @@ recording::type::get_aligned (size_t alignment_in_bytes)
   return result;
 }
 
+void
+recording::type::set_packed ()
+{
+  m_packed = true;
+}
+
 /* Given a type, get a vector version of the type.
 
    Implements the post-error-checking part of
@@ -3747,7 +3753,8 @@ recording::struct_::replay_into (replayer *r)
   set_playback_obj (
     r->new_compound_type (playback_location (r, get_loc ()),
 			  get_name ()->c_str (),
-			  true /* is_struct */));
+			  true, /* is_struct */
+			  m_packed));
 }
 
 const char *
@@ -3801,7 +3808,8 @@ recording::union_::replay_into (replayer *r)
   set_playback_obj (
     r->new_compound_type (playback_location (r, get_loc ()),
 			  get_name ()->c_str (),
-			  false /* is_struct */));
+			  false, /* is_struct */
+			  m_packed));
 }
 
 /* Implementation of recording::memento::make_debug_string for
@@ -3872,7 +3880,7 @@ recording::fields::replay_into (replayer *)
   playback_fields.create (m_fields.length ());
   for (unsigned i = 0; i < m_fields.length (); i++)
     playback_fields.safe_push (m_fields[i]->playback_field ());
-  m_struct_or_union->playback_compound_type ()->set_fields (&playback_fields);
+  m_struct_or_union->playback_compound_type ()->set_fields (&playback_fields, m_struct_or_union->m_packed);
 }
 
 /* Override the default implementation of
