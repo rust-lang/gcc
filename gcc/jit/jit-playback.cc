7e02bf4d96dc54eb786136af06f59830366383d6
Antoni Boucher <bouanto@zoho.com>
2023-06-26 18:29:15 -0400

    Add ability to get CPU features

--- gcc/jit/jit-playback.cc
+++ gcc/jit/jit-playback.cc
@@ -47,6 +47,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "jit-result.h"
 #include "jit-builtins.h"
 #include "jit-tempdir.h"
+#include "jit-target.h"
 
 #ifdef _WIN32
 #include "jit-w32.h"
@@ -3607,6 +3608,7 @@ replay ()
   JIT_LOG_SCOPE (get_logger ());
 
   init_types ();
+  jit_target_init ();
 
   /* Replay the recorded events:  */
   timevar_push (TV_JIT_REPLAY);
