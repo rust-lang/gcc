63cce91df900f5e5cf9bf80c6ad53480e157d894
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-07-21 11:03:59 +0200

    fixup! WIP: Add support for function attributes
    Add support for const attribute in libgccjit

--- gcc/jit/libgccjit.h
+++ gcc/jit/libgccjit.h
@@ -2106,6 +2106,7 @@ enum gcc_jit_fn_attribute
   GCC_JIT_FN_ATTRIBUTE_COLD,
   GCC_JIT_FN_ATTRIBUTE_RETURNS_TWICE,
   GCC_JIT_FN_ATTRIBUTE_PURE,
+  GCC_JIT_FN_ATTRIBUTE_CONST,
 };
 
 /* Add an attribute to a function.  */
