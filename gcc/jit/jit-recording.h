8eeac3ed9a658fc777afe1ad7db7cbeaf3c7fd50
tempdragon <645703113@qq.com>
2024-02-18 23:38:48 +0800

    feat(gccjit): Allow set_loccation to various classes
    Including:
    1. field
    2. function
    3. rvalue

--- gcc/jit/jit-recording.h
+++ gcc/jit/jit-recording.h
@@ -1068,6 +1068,7 @@ class array_type : public type
   bool is_signed () const final override { return false; }
 
   void replay_into (replayer *) final override;
+  void set_loc(location * loc) { m_loc = loc; }
 
  private:
   string * make_debug_string () final override;
@@ -1158,6 +1159,7 @@ public:
 
   compound_type * get_container () const { return m_container; }
   void set_container (compound_type *c) { m_container = c; }
+  void set_loc (location * loc) { m_loc = loc; }
 
   void replay_into (replayer *) override;
 
@@ -1232,6 +1234,7 @@ public:
   bool is_signed () const final override { return false; }
 
   bool has_known_size () const final override { return m_fields != NULL; }
+  void set_loc (location * loc) { m_loc = loc; }
 
   playback::compound_type *
   playback_compound_type ()
@@ -1373,6 +1376,7 @@ public:
   }
 
   location * get_loc () const { return m_loc; }
+  void set_loc (location * loc) { m_loc = loc; }
 
   /* Get the recording::type of this rvalue.
 
@@ -1569,6 +1573,7 @@ public:
   new_block (const char *name);
 
   location *get_loc () const { return m_loc; }
+  void set_loc (location * loc) { m_loc = loc; }
   type *get_return_type () const { return m_return_type; }
   string * get_name () const { return m_name; }
   const vec<param *> &get_params () const { return m_params; }
@@ -1704,6 +1709,7 @@ public:
   bool validate ();
 
   location *get_loc () const;
+  void set_loc (location * loc);
 
   statement *get_first_statement () const;
   statement *get_last_statement () const;
@@ -2513,6 +2519,7 @@ public:
 
   block *get_block () const { return m_block; }
   location *get_loc () const { return m_loc; }
+  void set_loc (location * loc) { m_loc = loc; }
 
 protected:
   statement (block *b, location *loc)
