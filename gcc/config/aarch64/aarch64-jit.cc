d17eb32540714d43314ccf68bf591a1f4fade9b9
winstonallo <arthurbiedchar@gmail.com>
2025-10-13 16:15:41 +0200

    Remove target-dependent type initializations from {aarch64,i386}-jit.cc

--- gcc/config/aarch64/aarch64-jit.cc
+++ gcc/config/aarch64/aarch64-jit.cc
@@ -58,24 +58,6 @@ aarch64_jit_register_target_info (void)
   }
 #endif
 
-  if (targetm.scalar_mode_supported_p (TImode))
-  {
-    jit_target_add_supported_target_dependent_type(GCC_JIT_TYPE_UINT128_T);
-    jit_target_add_supported_target_dependent_type(GCC_JIT_TYPE_INT128_T);
-  }
-
-  if (float16_type_node != NULL && TYPE_PRECISION(float16_type_node) == 16)
-    jit_target_add_supported_target_dependent_type(GCC_JIT_TYPE_FLOAT16);
-
-  if (float32_type_node != NULL && TYPE_PRECISION(float32_type_node) == 32)
-    jit_target_add_supported_target_dependent_type(GCC_JIT_TYPE_FLOAT32);
-
-  if (float64_type_node != NULL && TYPE_PRECISION(float64_type_node) == 64)
-    jit_target_add_supported_target_dependent_type(GCC_JIT_TYPE_FLOAT64);
-
-  if (float128_type_node != NULL && TYPE_PRECISION(float128_type_node) == 128)
-    jit_target_add_supported_target_dependent_type(GCC_JIT_TYPE_FLOAT128);
-
   if (TARGET_AES)
     jit_add_target_info ("target_feature", "aes");
   if (TARGET_BF16_FP)
