47b70db8779a0991b9370e571ab18901b2643a63
Jeevitha <jeevitha@linux.ibm.com>
2026-07-01 05:11:42 -0500

    testsuite: Update Future VSX arithmetic tests
    The existing tests use inline asm to force operands into FPRs so that
    Future VSX arithmetic instructions are generated.
    
    Remove the operand forcing and let the tests check the compiler's
    default instruction selection. This allows the tests to catch any
    future changes in instruction selection caused by alternative
    reordering.
    
    2026-07-01  Jeevitha Palanisamy  <jeevitha@linux.ibm.com>
    
    gcc/testsuite:
    	* gcc.target/powerpc/vsx_arith_builtin-1.c: Remove inline asm
    	that forces operands into FPRs.
    	* gcc.target/powerpc/vsx_arith_builtin-2.c: Likewise.
    	* gcc.target/powerpc/vsx_arith_vect_1.c: Likewiswe.

--- gcc/testsuite/gcc.target/powerpc/vsx_arith_builtin-1.c
+++ gcc/testsuite/gcc.target/powerpc/vsx_arith_builtin-1.c
@@ -10,56 +10,46 @@ typedef vector signed long long v2di_t;
 
 v4si_t int_add (v4si_t x, v4si_t y)
 {
-  /* force x & y into FPRs to prefer xvadduwm over vadduwm.  */
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_add (x, y);            /* xvadduwm */
 }
 
 v4si_t int_sub (v4si_t x, v4si_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_sub (x, y);            /* xvsubuwm */
 }
 
 v4si_t int_mul (v4si_t x, v4si_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mul (x, y);            /* xvmuluwm */
 }
 
 v4si_t int_mulhi (v4si_t x, v4si_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mulh (x, y);           /* xvmulhsw */
 }
 
 v8hi_t short_add (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_add (x, y);            /* xvadduhm */
 }
 
 v8hi_t short_sub (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_sub (x, y);            /* xvsubuhm */
 }
 
 v8hi_t short_mul (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mul (x, y);            /* xvmuluhm */
 }
 
 v8hi_t short_mulhi (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mulh (x, y);            /* xvmulhsh */
 }
 
 v2di_t longlong_mulhi (v2di_t x, v2di_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mulh (x, y);            /* vmulhsd */
 }
 
--- gcc/testsuite/gcc.target/powerpc/vsx_arith_builtin-2.c
+++ gcc/testsuite/gcc.target/powerpc/vsx_arith_builtin-2.c
@@ -10,56 +10,46 @@ typedef vector unsigned long long v2di_t;
 
 v4si_t uint_add (v4si_t x, v4si_t y)
 {
-  /* force x & y into FPRs to prefer xvadduwm over vadduwm.  */
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_add (x, y);            /* xvadduwm */
 }
 
 v4si_t uint_sub (v4si_t x, v4si_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_sub (x, y);            /* xvsubuwm */
 }
 
 v4si_t uint_mul (v4si_t x, v4si_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mul (x, y);            /* xvmuluwm */
 }
 
 v4si_t uint_mulhi (v4si_t x, v4si_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mulh (x, y);           /* xvmulhuw */
 }
 
 v8hi_t ushort_add (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_add (x, y);            /* xvadduhm */
 }
 
 v8hi_t ushort_sub (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_sub (x, y);            /* xvsubuhm */
 }
 
 v8hi_t ushort_mul (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mul (x, y);            /* xvmuluhm */
 }
 
 v8hi_t ushort_mulhi (v8hi_t x, v8hi_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mulh (x, y);            /* xvmulhuh */
 }
 
 v2di_t ulonglong_mulhi (v2di_t x, v2di_t y)
 {
-  __asm__ (" # %x0,%x0" : "+f" (x), "+f" (y));
   return vec_mulh (x, y);            /* vmulhud */
 }
 
