c8a5c1d6ed236aea3ff2012ee8947b48a303569b
Tomasz Kamiński <tkaminsk@redhat.com>
2026-07-10 16:27:34 +0200

    libstdc++: Fix typos in comment in start_lifetime_as/neg.cc.
    Fix typos in comment pointed by Jakub and remove trailing
    whitespaces.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/std/memory/start_lifetime_as/neg.cc: Fix
    	typos in comment and remove trailing whitespaces.

--- libstdc++-v3/testsuite/std/memory/start_lifetime_as/neg.cc
+++ libstdc++-v3/testsuite/std/memory/start_lifetime_as/neg.cc
@@ -16,26 +16,26 @@ struct NonTrivial
 void test(void* p)
 {
   (void)std::start_lifetime_as<Incomplete>(p);           // { dg-error "here" }
-  (void)std::start_lifetime_as_array<Incomplete>(p, 2);  // { dg-error "here" } 
+  (void)std::start_lifetime_as_array<Incomplete>(p, 2);  // { dg-error "here" }
   (void)std::start_lifetime_as<NonTrivial>(p);           // { dg-error "here" }
-  // Array are implicit-lifetime regardless of they element type
+  // Arrays are implicit-lifetime regardless of their element type
   (void)std::start_lifetime_as_array<NonTrivial>(p, 2);
 
   const void* cp = p;
   (void)std::start_lifetime_as<Incomplete>(cp);           // { dg-error "here" }
-  (void)std::start_lifetime_as_array<Incomplete>(cp, 2);  // { dg-error "here" } 
+  (void)std::start_lifetime_as_array<Incomplete>(cp, 2);  // { dg-error "here" }
   (void)std::start_lifetime_as<NonTrivial>(cp);           // { dg-error "here" }
   (void)std::start_lifetime_as_array<NonTrivial>(cp, 2);
 
   volatile void* vp = p;
   (void)std::start_lifetime_as<Incomplete>(vp);           // { dg-error "here" }
-  (void)std::start_lifetime_as_array<Incomplete>(vp, 2);  // { dg-error "here" } 
+  (void)std::start_lifetime_as_array<Incomplete>(vp, 2);  // { dg-error "here" }
   (void)std::start_lifetime_as<NonTrivial>(vp);           // { dg-error "here" }
   (void)std::start_lifetime_as_array<NonTrivial>(vp, 2);
 
   const volatile void* cvp = vp;
   (void)std::start_lifetime_as<Incomplete>(cvp);           // { dg-error "here" }
-  (void)std::start_lifetime_as_array<Incomplete>(cvp, 2);  // { dg-error "here" } 
+  (void)std::start_lifetime_as_array<Incomplete>(cvp, 2);  // { dg-error "here" }
   (void)std::start_lifetime_as<NonTrivial>(cvp);           // { dg-error "here" }
   (void)std::start_lifetime_as_array<NonTrivial>(cvp, 2);
 }
